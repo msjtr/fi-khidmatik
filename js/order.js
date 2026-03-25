3a0c7e07d3d6d6c18b652064a7bd7bb02ad58c3b
msjtr <msjt301@gmail.com>
2026-03-25 15:12:20 +0300

    Update order.js

--- js/order.js
+++ js/order.js
@@ -1,1 +1,31 @@
+function checkout(){
 
+let orders = JSON.parse(localStorage.getItem("orders")) || [];
+
+let order = {
+id: Date.now(),
+
+customer: document.getElementById("name").value,
+phone: document.getElementById("phone").value,
+address: document.getElementById("address").value,
+
+cart: cart,
+
+payment: document.getElementById("payment").value,
+tamara_auth: document.getElementById("tamara_auth").value,
+tamara_order: document.getElementById("tamara_order").value,
+
+shipping: document.getElementById("shipping").value,
+
+status: "جديد",
+date: new Date().toISOString()
+};
+
+orders.push(order);
+
+localStorage.setItem("orders", JSON.stringify(orders));
+localStorage.setItem("currentOrder", orders.length - 1);
+
+window.location.href = "invoice.html";
+
+}
