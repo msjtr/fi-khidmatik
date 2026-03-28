ccabb1bd2bbf451ce6db83b168d3485d84ac8213
msjtr <msjt301@gmail.com>
2026-03-28 22:51:43 +0300

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,44 +1,40 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+
 import {
-getFirestore,
-collection,
-addDoc,
-doc,
-getDoc,
-getDocs,
-updateDoc,
-deleteDoc,
-setDoc,
-query,
-orderBy,
-where,
-limit
+    getFirestore,
+    collection,
+    addDoc,
+    doc,
+    getDoc,
+    getDocs,
+    updateDoc,
+    deleteDoc,
+    setDoc
 } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
+// إعدادات Firebase
 const firebaseConfig = {
-apiKey: "AIzaSyBWYW6Qqlhh904pBeuJ29wY7Cyjm2uklBA",
-authDomain: "msjt301-974bb.firebaseapp.com",
-projectId: "msjt301-974bb",
-storageBucket: "msjt301-974bb.firebasestorage.app",
-messagingSenderId: "186209858482",
-appId: "1:186209858482:web:186ca610780799ef562aab"
+    apiKey: "AIzaSyBWYW6Qqlhh904pBeuJ29wY7Cyjm2uklBA",
+    authDomain: "msjt301-974bb.firebaseapp.com",
+    projectId: "msjt301-974bb",
+    storageBucket: "msjt301-974bb.firebasestorage.app",
+    messagingSenderId: "186209858482",
+    appId: "1:186209858482:web:186ca610780799ef562aab"
 };
 
+// تشغيل Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
+// التصدير
 export {
-db,
-collection,
-addDoc,
-doc,
-getDoc,
-getDocs,
-updateDoc,
-deleteDoc,
-setDoc,
-query,
-orderBy,
-where,
-limit
+    db,
+    collection,
+    addDoc,
+    doc,
+    getDoc,
+    getDocs,
+    updateDoc,
+    deleteDoc,
+    setDoc
 };
