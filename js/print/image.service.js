ed974361e1e34813269728b7d0427c30358dad37
msjtr <msjt301@gmail.com>
2026-03-30 05:39:01 +0300

    Update image.service.js

--- js/print/image.service.js
+++ js/print/image.service.js
@@ -1,1 +1,18 @@
+export async function saveImage(element) {
 
+const canvas = await html2canvas(element, { scale: 2 });
+
+let quality = 0.7;
+let data;
+
+do {
+    data = canvas.toDataURL('image/jpeg', quality);
+    quality -= 0.05;
+} while (data.length > 200000);
+
+const link = document.createElement('a');
+link.download = "invoice.jpg";
+link.href = data;
+link.click();
+
+}
