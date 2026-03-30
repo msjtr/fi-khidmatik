96f61117cfdf82cd99aff9599a5551d2a1c84950
msjtr <msjt301@gmail.com>
2026-03-30 09:43:26 +0300

    Update pdf.service.js

--- js/print/pdf.service.js
+++ js/print/pdf.service.js
@@ -1,16 +1,16 @@
-export async function savePDF(element) {
+export async function generatePDF(element, order) {
 
-const canvas = await html2canvas(element, {
-    scale: 3,
-    backgroundColor: "#fff"
-});
+    const canvas = await html2canvas(element, {
+        scale: 3,
+        backgroundColor: "#fff"
+    });
 
-const img = canvas.toDataURL("image/jpeg", 1);
+    const img = canvas.toDataURL("image/jpeg", 1);
 
-const { jsPDF } = window.jspdf;
-const pdf = new jsPDF();
+    const { jsPDF } = window.jspdf;
+    const pdf = new jsPDF();
 
-pdf.addImage(img, 'JPEG', 0, 0, 210, 297);
-pdf.save("invoice.pdf");
+    pdf.addImage(img, 'JPEG', 0, 0, 210, 297);
 
+    pdf.save(`invoice_${order?.orderNumber || 'file'}.pdf`);
 }
