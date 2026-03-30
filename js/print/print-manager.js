3899ab15f8e6504f8d08859289352863c2642692
msjtr <msjt301@gmail.com>
2026-03-30 05:41:23 +0300

    Update print-manager.js

--- js/print/print-manager.js
+++ js/print/print-manager.js
@@ -1,1 +1,24 @@
+import { savePDF } from './pdf.service.js';
+import { saveImage } from './image.service.js';
+import { printHTML } from './print.service.js';
 
+export function handlePrint(type) {
+
+const el = document.getElementById('invoiceToPrint');
+
+if (!el) return alert('لا توجد فاتورة');
+
+switch(type) {
+
+case 'pdf':
+    return savePDF(el);
+
+case 'image':
+    return saveImage(el);
+
+case 'print':
+    return printHTML(el);
+
+}
+
+}
