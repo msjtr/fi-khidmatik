96c3cc9829759d88d43020d6d7cf44219834c3cc
msjtr <msjt301@gmail.com>
2026-03-31 05:33:18 +0300

    Update print-manager.js

--- js/print/print-manager.js
+++ js/print/print-manager.js
@@ -2,9 +2,9 @@ import { generatePDF } from './pdf.service.js';
 import { generateImage } from './image.service.js';
 import { printInvoice } from './print.service.js';
 
-export function handlePrint(type) {
+export function handlePrint(type, order) {
 
-    const el = document.getElementById('invoiceToPrint');
+    const el = document.querySelector('.invoice');
 
     if (!el) {
         alert('لا توجد فاتورة');
@@ -14,10 +14,10 @@ export function handlePrint(type) {
     switch(type) {
 
         case 'pdf':
-            return generatePDF(el);
+            return generatePDF(el, order);
 
         case 'image':
-            return generateImage(el);
+            return generateImage(el, order);
 
         case 'print':
             return printInvoice(el);
