506cf9b36018dfc5ace8f51eededca5fe23f1e69
msjtr <msjt301@gmail.com>
2026-03-31 15:33:04 +0300

    Update print.service.js

--- js/print/print.service.js
+++ js/print/print.service.js
@@ -1,32 +1,8 @@
-export function printInvoice(element) {
-
-    const win = window.open('', '_blank');
-
-    const base = window.location.origin;
-
-    win.document.write(`
-    <html dir="rtl">
-    <head>
-        <meta charset="UTF-8">
-        <title>فاتورة</title>
-
-        <link rel="stylesheet" href="${base}/css/design.css">
-        <link rel="stylesheet" href="${base}/css/print.css">
-    </head>
-
-    <body style="direction: rtl; margin:0;">
-        ${element.outerHTML}
-    </body>
-    </html>
-    `);
-
-    win.document.close();
-
-    win.onload = () => {
-        setTimeout(() => {
-            win.focus();
-            win.print();
-            win.close();
-        }, 300);
-    };
+// print.service.js
+export async function printInvoice(element) {
+    // طباعة العنصر الحالي (يفتح نافذة الطباعة مباشرة)
+    const originalTitle = document.title;
+    document.title = 'طباعة الفاتورة';
+    window.print();
+    document.title = originalTitle;
 }
