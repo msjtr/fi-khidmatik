4f86f484cc9915b94b69f466f8bbf17f606628f9
msjtr <msjt301@gmail.com>
2026-03-31 05:34:35 +0300

    Update pdf.service.js

--- js/print/pdf.service.js
+++ js/print/pdf.service.js
@@ -8,34 +8,38 @@ export async function generatePDF(element, order) {
         format: 'a4'
     });
 
-    // 👇 نحسب المقاسات الصح
     const pageWidth = 210;
     const pageHeight = 297;
 
     const canvas = await html2canvas(element, {
         scale: 2,
         useCORS: true,
-        scrollY: -window.scrollY
+        backgroundColor: "#ffffff"
     });
-
-    const imgData = canvas.toDataURL('image/jpeg', 1.0);
 
     const imgWidth = pageWidth;
     const imgHeight = (canvas.height * imgWidth) / canvas.width;
 
-    let heightLeft = imgHeight;
-    let position = 0;
+    const imgData = canvas.toDataURL('image/jpeg', 1.0);
 
-    // 🔥 تقسيم احترافي بدون قص
-    while (heightLeft > 0) {
+    let y = 0;
 
-        pdf.addImage(imgData, 'JPEG', 0, position, imgWidth, imgHeight);
+    // 🔥 تقسيم فعلي
+    while (y < imgHeight) {
 
-        heightLeft -= pageHeight;
+        pdf.addImage(
+            imgData,
+            'JPEG',
+            0,
+            -y,
+            imgWidth,
+            imgHeight
+        );
 
-        if (heightLeft > 0) {
+        y += pageHeight;
+
+        if (y < imgHeight) {
             pdf.addPage();
-            position -= pageHeight;
         }
     }
 
