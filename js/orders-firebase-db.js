b7298ebd255dcb6272924f0c1adcbc1a32f77fc7
msjtr <msjt301@gmail.com>
2026-04-13 01:34:38 +0300

    Update orders-firebase-db.js

--- js/orders-firebase-db.js
+++ js/orders-firebase-db.js
@@ -1,4 +1,4 @@
-// orders-firebase-db.js
+// js/orders-firebase-db.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-firestore.js";
 
