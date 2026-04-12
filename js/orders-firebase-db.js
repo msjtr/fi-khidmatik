046c6ee1f062389b8513a61e72b6f68da66d472a
msjtr <msjt301@gmail.com>
2026-04-13 01:11:01 +0300

    Create orders-firebase-db.js

---
+++ js/orders-firebase-db.js
@@ -0,0 +1,15 @@
+// orders-firebase-db.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-firestore.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBWYW6Qqlhh904pBeuJ29wY7Cyjm2uklBA",
+    authDomain: "msjt301-974bb.firebaseapp.com",
+    projectId: "msjt301-974bb",
+    storageBucket: "msjt301-974bb.firebasestorage.app",
+    messagingSenderId: "186209858482",
+    appId: "1:186209858482:web:186ca610780799ef562aab"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
