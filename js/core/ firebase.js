c6e6270d5ab506d11f172036c3896addf2334dc7
msjtr <msjt301@gmail.com>
2026-04-15 21:13:51 +0300

    Update  firebase.js

--- js/core/ firebase.js
+++ js/core/ firebase.js
@@ -1,6 +1,10 @@
+// js/core/firebase.js
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.12.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.12.0/firebase-firestore.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.12.0/firebase-analytics.js";
+
+// تأكد أن ملف config.js موجود فعلياً داخل مجلد js/core/ بجانب هذا الملف
 import { firebaseConfig } from './config.js';
 
 const app = initializeApp(firebaseConfig);
