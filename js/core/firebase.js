f63d6290cb485007645b3885766b4e76d5c38e78
msjtr <msjt301@gmail.com>
2026-04-15 23:05:17 +0300

    Update firebase.js

--- js/core/firebase.js
+++ js/core/firebase.js
@@ -1,15 +1,9 @@
 // js/core/firebase.js
-
-// الاستيراد باستخدام الروابط الكاملة لمنع خطأ TypeError
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.12.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.12.0/firebase-firestore.js";
-
-// استيراد الإعدادات من الملف المحلي (تأكد من وجود export داخل config.js)
 import { firebaseConfig } from './config.js';
 
-// تهيئة التطبيق وقاعدة البيانات
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-// تصدير المتغير ليستخدمه موديول الطلبات والعملاء
 export { db };
