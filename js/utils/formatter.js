ce7e7622e8fb0307cd62cc50508260d0550a7c99
msjtr <msjt301@gmail.com>
2026-04-15 16:52:55 +0300

    Update formatter.js

--- js/utils/formatter.js
+++ js/utils/formatter.js
@@ -1,1 +1,9 @@
+export const formatCurrency = (amount, currency = 'ريال') => {
+    return `${Number(amount).toFixed(2)} ${currency}`;
+};
 
+export const formatDate = (timestamp) => {
+    if (!timestamp) return '';
+    const date = timestamp.toDate ? timestamp.toDate() : new Date(timestamp);
+    return date.toLocaleDateString('ar-EG');
+};
