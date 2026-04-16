9cf4c5c027515a13b989e00366efe70f660ddbbb
msjtr <msjt301@gmail.com>
2026-04-17 02:37:04 +0300

    Update firebase.js

--- js/core/firebase.js
+++ js/core/firebase.js
@@ -1,9 +1,7 @@
 // js/core/firebase.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.12.0/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/12.12.0/firebase-firestore.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 import { firebaseConfig } from './config.js';
 
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
-export { db };
+export const db = getFirestore(app);
