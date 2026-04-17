4b5ccea892b08866849dee7dc2b90debadff34f7
msjtr <msjt301@gmail.com>
2026-04-17 14:17:50 +0300

    Update firebase.js

--- js/core/firebase.js
+++ js/core/firebase.js
@@ -1,10 +1,6 @@
-// js/core/firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 import { firebaseConfig } from './config.js';
 
-// تهيئة Firebase
 const app = initializeApp(firebaseConfig);
-
-// تصدير db بشكل صريح ومباشر
 export const db = getFirestore(app);
