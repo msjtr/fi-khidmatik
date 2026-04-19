78304bf7a436a4cd2a67c5bb81c54835f9e3ff15
msjtr <msjt301@gmail.com>
2026-04-19 18:57:41 +0300

    Update formatter.js

--- js/utils/formatter.js
+++ js/utils/formatter.js
@@ -1,10 +1,10 @@
+/**
+ * المسار الحالي: js/utils/formatter.js
+ */
+
 export const formatCurrency = (amount) => {
     return new Intl.NumberFormat('en-US', {
         style: 'currency',
         currency: 'SAR',
     }).format(amount);
-};
-
-export const toEnglishDigits = (str) => {
-    return str.replace(/[٠-٩]/g, (d) => "٠١٢٣٤٥٦٧٨٩".indexOf(d));
 };
